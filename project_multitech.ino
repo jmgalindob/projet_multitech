83e14535940f15b2cb09e544f775ad277448402f
jmgalindob <jmgalindob@eafit.edu.co>
2023-03-13 08:29:45 +0100

    Premier templade du code

---
+++ project_multitech.ino
@@ -0,0 +1,13 @@
+// Ce code concene le protype du roject mutlitech: parking automatisé 
+// Il est realiser par: Jose Miguel GALNDO BARCO et Sebastian PRUNIER
+// Date de debut: 13/03/2023
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
