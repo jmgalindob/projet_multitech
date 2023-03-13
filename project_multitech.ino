d9ce8462426553b98a8baa309b34e00f841834d6
jmgalindob <jmgalindob@eafit.edu.co>
2023-03-13 08:34:40 +0100

    modification du template

--- project_multitech.ino
+++ project_multitech.ino
@@ -1,13 +1,76 @@
-// Ce code concene le protype du roject mutlitech: parking automatisé 
-// Il est realiser par: Jose Miguel GALNDO BARCO et Sebastian PRUNIER
-// Date de debut: 13/03/2023
+//-------------------------------------------------
+//-------------- DESCRIPTION-----------------------
+//-------------------------------------------------
 
-void setup() {
-  // put your setup code here, to run once:
+////////////////////////////////////////////////////
+/// Titre: Parking automatique
+////////////////////////////////////////////////////
+/// Carte: Arduino MEGA
+////////////////////////////////////////////////////
+//  Date/version: 03/2023
+//  Description: Gestion des entrées/sorties d'un parking automatique
+///////////////////////////////////////////////////
+
+//-------------------------------------------------
+//--------- DIRECTIVES PREPROCESSSEUR--------------
+//-------------------------------------------------
+
+//////////////////////////////////////////////////////////////
+
+// Inclusion de bibliothèques
+
+#include <Servo.h>
+
+
+// Définition des broches...
+
+#define   
+
+        
+//-------------------------------------------------
+//--------- DECLARATIONS --------------------------
+//-------------------------------------------------
+
+  // Déclaration des variables
+int ETAT=0;
+
+  // Déclaration des fonctions
+
+
+    // Instantation d'objet
+Servo Servoentree;
+Servo Servosortie;
+
+void setup() 
+     {      
+        //configuration 
+  pinMode(A, OUTPUT);
+
+  
+        //initialisation
+ Servoentree.attach(Entree);
+ Servoentree.write(0);
+ Servosortie.attach(Sortie);
+ Servosortie.write(0);
 
 }
+//-------------------------------------------------
+//------------- PROCEDURE ET FONCTIONS -----------
+//-------------------------------------------------
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//  Code des procédures et fonctions
 
-}
+
+//-------------------------------------------------
+//------------- BOUCLE INFINIE --------------------
+//-------------------------------------------------
+
+void loop() 
+{
+  enum liste_etat{};
+  switch(ETAT){
+    case
+    
+
+       }    
+   }
